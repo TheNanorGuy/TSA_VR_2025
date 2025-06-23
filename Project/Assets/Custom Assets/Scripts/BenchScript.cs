5c8e7cd7da5f2731603d87d536f5ee5f6aec74d6
TheNanorGuy <thenanorguy@gmail.com>
2025-06-23 12:37:16 -0700

    AddedAnimToWheel

--- Project/Assets/Custom Assets/Scripts/BenchScript.cs
+++ Project/Assets/Custom Assets/Scripts/BenchScript.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class BenchScript : MonoBehaviour
 {
     Quaternion rot;
+    Quaternion lRot;
+    public GameObject wheel;
     // Start is called before the first frame update
     void Start()
     {
         rot = gameObject.transform.rotation;
+        lRot = gameObject.transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rot = Quaternion.Euler(rot.eulerAngles.x, rot.eulerAngles.y, 0);
+        Debug.Log("Bench angle is " + rot.eulerAngles.z);
+        rot = Quaternion.Euler(rot.eulerAngles.x, rot.eulerAngles.y, rot.eulerAngles.z + wheel.transform.rotation.eulerAngles.z);
+        gameObject.transform.rotation = rot;
     }
 }
--- Project/Assets/Custom Assets/Scripts/FerrisWheelScript.cs
+++ Project/Assets/Custom Assets/Scripts/FerrisWheelScript.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 public class FerrisWheelScript : MonoBehaviour
 {
     public Animator animator;
+    BenchScript bench;
     // Start is called before the first frame update
     void Start()
     {
+        bench = FindObjectOfType<BenchScript>();
         StartCoroutine(Begin());
     }
 
